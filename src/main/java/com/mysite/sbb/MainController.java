48494c9d7e8f8f70b3a5efcb9ce7d0b4d9b332ab
Karryun <gdy0210@naver.com>
2026-03-08 17:40:13 +0900

    update

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,16 +2,19 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public void home(){
+    @ResponseBody // 리턴 값이 응답의 본문 (고객이 볼 수 있는게 return);
+    public int home(){
         System.out.println("Home");
-        this.contact();
+        return 22;
     }
 
     @GetMapping("/about")
+    @ResponseBody
     public void about(){
         System.out.println("about");
         this.contact();
